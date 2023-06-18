63a532421793d6b5162496635e95497ce5fee661
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2023-06-18 22:57:58 +0900

    make appbar
    1. make app-bar
    2. make menu router btn

--- src/components/LolvsHeader.vue
+++ src/components/LolvsHeader.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    헤더
+    <!-- S: 상단 메뉴바 -->
+    <v-app-bar class="header_appbar" elevate-on-scroll height="80">
+      <div class="contents">
+        <div class="title_wrapper">
+          <v-toolbar-title class="title" @click="moveRoute('/')">
+            <div class="wrapper">
+              <div class="focus">
+                LOLVS
+              </div>
+              <div class="mask">
+                <div class="text">LOLVS</div>
+              </div>
+            </div>
+          </v-toolbar-title>
+        </div>
+
+        <div class="menu_wrapper color_white">
+          <v-breadcrumbs :items="menuItems" large/>
+        </div>
+      </div>
+    </v-app-bar>
+    <!-- E: 상단 메뉴바 -->
   </div>
 </template>
 
@@ -10,6 +31,23 @@ export default {
   name: "LolvsHeader",
   data () {
     return {
+      menuItems : [
+        {
+          text: '리그별 통계',
+          disabled: false,
+          href: '/a',
+        },
+        {
+          text: '선수별 통계',
+          disabled: false,
+          href: '/b',
+        },
+        {
+          text: '챔피언별 통계',
+          disabled: false,
+          href: '/c',
+        },
+      ]
     }
   },
   watch: {
