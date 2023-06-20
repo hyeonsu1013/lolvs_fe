8146660c840f4403ae9448a42ea6f6434f07b962
hyeonsu1013 <71484658+hyeonsu1013@users.noreply.github.com>
2023-06-20 22:20:32 +0900

    modify lolvsHeader css

--- src/components/LolvsHeader.vue
+++ src/components/LolvsHeader.vue
@@ -4,7 +4,7 @@
     <v-app-bar class="header_appbar" elevate-on-scroll height="80">
       <div class="contents">
         <div class="title_wrapper">
-          <v-toolbar-title class="title" @click="moveRoute('/')">
+          <v-toolbar-title class="title" @click="moveRouter(mainItem)">
             <div class="wrapper">
               <div class="focus">
                 LOLVS
@@ -17,8 +17,15 @@
         </div>
 
         <div class="menu_wrapper color_white">
-          <v-breadcrumbs :items="menuItems" large>
-            <template v-slot:divider> | </template>
+          <v-breadcrumbs :items="menuItems" large divider=" | ">
+            <template v-slot:item="{ item }">
+              <v-breadcrumbs-item class="click_pointer"
+                  :class="{fbold : currentMenu == item.order}"
+                  @click="moveRouter(item)"
+                  :disabled="item.disabled">
+                {{ item.text }}
+              </v-breadcrumbs-item>
+            </template>
           </v-breadcrumbs>
         </div>
       </div>
@@ -33,21 +40,29 @@ export default {
   name: "LolvsHeader",
   data () {
     return {
+      currentMenu : -1,
+      mainItem : {
+        domain: '/',
+        order : -1,
+      },
       menuItems : [
         {
           text: '리그별 통계',
           disabled: false,
-          href: '/league',
+          domain: '/league',
+          order : 1,
         },
         {
           text: '선수별 통계',
           disabled: false,
-          href: '/pro',
+          domain: '/pro',
+          order : 2,
         },
         {
           text: '챔피언별 통계',
           disabled: false,
-          href: '/champion',
+          domain: '/champion',
+          order : 3,
         },
       ]
     }
@@ -57,8 +72,22 @@ export default {
   computed : {
   },
   methods: {
+    moveRouter(item) {
+      this.currentMenu = item.order;
+
+      this.$router.push({
+          path: item.domain,
+      });
+    },
   },
   created() {
+    // 새로고침 시 현재 메뉴 위치 파악
+    let path = this.$route.path;
+    this.menuItems.forEach(e => {
+      if(e.domain == path){
+        this.currentMenu = e.order;
+      }
+    });
   },
 }
 </script>
